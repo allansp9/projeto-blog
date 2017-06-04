cf3f2055a29624fb694db9045dee9d0ee699544f
Allan S <allansp9@gmail.com>
2017-06-04 03:36:20 +0000

    criacao de posts adicionada

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -24,4 +24,23 @@
 			$this->load->view('posts/view', $data);
 			$this->load->view('includes/footer');
 		}
+		
+		public function create(){
+		    $data['title'] = 'create post';
+		    
+		    $this->form_validation->set_rules('title', 'Título', 'required');
+		    $this->form_validation->set_rules('body', 'Corpo', 'required');
+		    
+		    if($this->form_validation->run() === FALSE){
+		        $this->load->view('includes/header');
+			    $this->load->view('posts/create', $data);
+			    $this->load->view('includes/footer'); 
+		        
+		    }else{
+		        $this->post_model->create_post();
+		        redirect('posts');
+		    }
+		    
+		    
+		}
 	}
